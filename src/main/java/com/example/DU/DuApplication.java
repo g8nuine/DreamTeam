1d507eeb95aa8b9dea2891b3a268830052ae85dd
Andrei Holub <holubgetrekt@gmail.com>
2022-02-18 16:03:36 +0100

    Initial commit

---
+++ src/main/java/com/example/DU/DuApplication.java
@@ -0,0 +1,13 @@
+package com.example.DU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuApplication.class, args);
+	}
+
+}
