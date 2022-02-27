52a1a2574780a4630510fa294e8a761c53130b36
Andrei Holub <holubgetrekt@gmail.com>
2022-02-27 16:52:19 +0100

    Customer services are done

---
+++ src/main/java/com/example/DU/Customer.java
@@ -0,0 +1,20 @@
+package com.example.DU;
+
+public class Customer {
+    public long id;
+    public String meno;
+    public String priezvisko;
+    public String email;
+
+    public Customer(long id, String meno, String priezvisko, String email) {
+        this.id = id;
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+        this.email = email;
+    }
+    public Customer() {
+
+    }
+}
+
+
